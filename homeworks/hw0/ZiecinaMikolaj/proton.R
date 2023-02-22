462a033d858c0fcce307935a1713dc3fbcfc95af
m-ziecina <125915114+m-ziecina@users.noreply.github.com>
2023-02-22 15:41:12 +0100

    [PD0] Zięcina Mikołaj

---
+++ homeworks/hw0/ZiecinaMikolaj/proton.R
@@ -0,0 +1,33 @@
+install.packages("proton")
+library(proton)
+proton()
+
+seq(-pi, pi, len = 5)
+
+rep(x = c(pi, -pi), times = 2)
+
+
+x = 'siemano'
+
+if (x == 'siemano') {
+  y = 'no elo elo'
+} else {
+  y = '...'
+}
+print(y)
+
+
+
+help(proton)
+proton()
+df <- employees
+
+df[df$surname == 'Pietraszko', ]
+proton(action = 'login', login='johnins')
+top1000passwords
+for (i in 1:1000) {
+  proton(action = 'login', login='johnins', password=passwords[i])
+}
+logs
+logs <- logs
+logs[logs$login == 'slap', ]
