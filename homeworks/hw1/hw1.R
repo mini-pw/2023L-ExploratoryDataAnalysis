040b30f0351b30ee7a305d538fb272b407803b5d
kozaka93 <anna1993kozak@gmail.com>
2023-02-27 16:22:08 +0100

    Create hw1.R

---
+++ homeworks/hw1/hw1.R
@@ -0,0 +1,98 @@
+library(PogromcyDanych)
+data(auta2012)
+
+# 1. Rozważając tylko obserwacje z PLN jako walutą (nie zważając na 
+# brutto/netto): jaka jest mediana ceny samochodów, które mają napęd elektryczny?
+
+
+
+# Odp:
+
+
+
+# 2. W podziale samochodów na marki oraz to, czy zostały wyprodukowane w 2001 
+# roku i później lub nie, podaj kombinację, dla której mediana liczby koni
+# mechanicznych (KM) jest największa.
+
+
+
+# Odp:
+
+
+
+# 3. Spośród samochodów w kolorze szary-metallic, których cena w PLN znajduje się
+# pomiędzy jej średnią a medianą (nie zważając na brutto/netto), wybierz te, 
+# których kraj pochodzenia jest inny niż kraj aktualnej rejestracji i poodaj ich liczbę.
+# UWAGA: Nie rozpatrujemy obserwacji z NA w kraju aktualnej rejestracji
+
+
+
+# Odp:
+
+
+
+# 4. Jaki jest rozstęp międzykwartylowy przebiegu (w kilometrach) Passatów
+# w wersji B6 i z benzyną jako rodzajem paliwa?
+
+
+
+# Odp:
+
+
+
+# 5. Biorąc pod uwagę samochody, których cena jest podana w koronach czeskich,
+# podaj średnią z ich ceny brutto.
+# Uwaga: Jeśli cena jest podana netto, należy dokonać konwersji na brutto (podatek 2%).
+
+
+
+# Odp:
+
+
+
+# 6. Których Chevroletów z przebiegiem większym niż 50 000 jest więcej: tych
+# ze skrzynią manualną czy automatyczną? Dodatkowo, podaj model, który najczęściej
+# pojawia się w obu przypadkach.
+
+
+
+# Odp:
+
+
+
+# 7. Jak zmieniła się mediana pojemności skokowej samochodów marki Mercedes-Benz,
+# jeśli weźmiemy pod uwagę te, które wyprodukowano przed lub w roku 2003 i po nim?
+
+
+
+# Odp:
+
+
+
+# 8. Jaki jest największy przebieg w samochodach aktualnie zarejestrowanych w
+# Polsce i pochodzących z Niemiec?
+
+
+
+# Odp:
+
+
+
+# 9. Jaki jest drugi najmniej popularny kolor w samochodach marki Mitsubishi
+# pochodzących z Włoch?
+
+
+
+# Odp:
+
+
+
+# 10. Jaka jest wartość kwantyla 0.25 oraz 0.75 pojemności skokowej dla 
+# samochodów marki Volkswagen w zależności od tego, czy w ich wyposażeniu 
+# dodatkowym znajdują się elektryczne lusterka?
+
+
+
+# Odp:
+
+
